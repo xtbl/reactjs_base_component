a3cc4bee681d577323e84e3f372183c5e7577f54
Cristobal Avila <cavila@spredfast.com>
2014-10-26 22:08:51 -0600

    first commit react base

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: 'www-root'
+                }
+            },
+            keepalive: true
+        }
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', 'connect', []);
+
+
+
+};
